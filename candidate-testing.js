113221568d950092d472860e78def4ea11631dbe
Pooja Bhatia Ahuja <34378156+Pooja1974@users.noreply.github.com>
2021-06-11 16:33:54 +0000

    part2 3

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,8 +8,8 @@ let candidateName=" ";
 let question = "Who was the first American woman in space?";
 let correctAnswer="Sally Ride";
 let candidateAnswer= " ";
-let questions = ["1) Who was the first American woman in space?",
-"2) True or false: 5 kilometer==5000 meters?", "3) (5+3)/2*10=?","4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","5) What is the minimum crew size for the ISS?" ];
+let questions = [" Who was the first American woman in space?",
+"True or false: 5 kilometer==5000 meters?", "3) (5+3)/2*10=?"," Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","What is the minimum crew size for the ISS?" ];
 let correctAnswers = ["Sally Ride","true","40","Trajectory","3"];
  let candidateAnswers = [];
 
@@ -58,6 +58,7 @@ console.log("Your Answer: " +candidateAnswers[j]);
 }
   let result =" ";
  let grade=((correctCount)/(totalquestion))*100;
+
 if(grade >= 80)
 result ="Passed"
 else
@@ -66,11 +67,7 @@ result="Failed";
 console.log(">>> Overall Grade: "+grade+"% ("+ correctCount +" out of " + questions.length+") <<<");
 
 console.log(">> Status: "+ result +" <<");
-
-
-
-
-  return result;
+ return result;
 }
 
 function runProgram() {
